be0c26fa51c141583e0b326eeb7b6957f47e45ae
Jean <jeancarlos.ramos@live.com>
2021-10-25 22:09:52 -0300

    Update 3_spinner_MainActivity.java

--- 3_spinner_MainActivity.java
+++ 3_spinner_MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.hamburgueriax;
+package com.example.atividadespinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		
-		Spinner spinner = (Spinner) findViewById(R.id.spinnerAno);
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context: this, 
-			R.array.years_array, android.R.layout.simple_spinner_dropdown_item);
-			
-		spinner.setAdapter(adapter);
-    }
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerAno);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.years_array, android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
 }
